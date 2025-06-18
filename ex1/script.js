13ad73a99c84fde5624a6ff5e33fa94d4f9b8762
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-06-18 11:22:46 -0300

    Add files via upload

---
+++ ex1/script.js
@@ -0,0 +1,8 @@
+function num(){
+    const numero=document.getElementById("numero").value;
+    if( numero % 2 == 0){
+        alert('Seu número é par');
+    }else{
+        alert('Seu número é ímpar');
+    }
+}
